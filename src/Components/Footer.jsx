972ada1ccd98d0aff083bde8509377447df7e810
jesusmh <jesuseduard4@gmail.com>
2023-06-22 22:43:51 -0400

    Add to footer container and center items

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,9 +1,8 @@
-import React from "react";
 import Github from "../images/Github.jsx";
 
 function Footer() {
   return (
-    <footer className="absolute bottom-1 text-2xl text-cyan-600">
+    <footer className="container flex flex-col justify-center items-center bottom-0 text-2xl text-cyan-600">
       <Github /> Hecho por Jesusmh
     </footer>
   );
